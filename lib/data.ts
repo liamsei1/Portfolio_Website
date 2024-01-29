f8f8838c28aef522385030becbbbd00fec26488c
liamsei1 <liamgabriely@gmail.com>
2024-01-29 16:59:22 +0200

    fix 2

--- lib/data.ts
+++ lib/data.ts
@@ -3,8 +3,8 @@ import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
 import cpsImg from "@/public/CPS_image.jpeg";
-import actImg from "@/public/ACT_image.PNG";
-import portfolioImg from "@/public/Portfolio_image.PNG";
+import actImg from "@/public/ACT_image.jpg";
+import portfolioImg from "@/public/Portfolio_image.jpg";
 
 export const links = [
   {
